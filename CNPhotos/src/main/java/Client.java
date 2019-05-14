57aefe7c20c8dfa4b24ae773df2e1a97c6c82605
PTTANK <nuno_faiao@hotmail.com>
2019-05-14 10:44:57 +0100

    + comandos

--- CNPhotos/src/main/java/Client.java
+++ CNPhotos/src/main/java/Client.java
@@ -1,14 +1,22 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Client {
 
     private static HashMap<Integer, Command> commandHashMap = new HashMap<>();
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        printCommands();
+        int input = -1;
+        while((input = scanner.nextInt()) != 99) {
+            Command cmd = commandHashMap.get(input);
+            cmd.execute();
+        }
     }
 
-    public void printCommands(){
+    public static void printCommands(){
         System.out.println("Available commands:");
         for(int idx : commandHashMap.keySet()){
             System.out.println(String.format("%d : %s", idx, commandHashMap.get(idx).commandTitle));
@@ -16,4 +24,5 @@ public class Client {
         System.out.println("99 : Exit");
         System.out.print("> ");
     }
+
 }
